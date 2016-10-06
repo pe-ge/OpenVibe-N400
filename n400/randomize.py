a0589c5ab34098d57167a328b100d0f92196c47d
pe-ge <gergelp@gmail.com>
2016-10-06 21:21:52 +0200

    included generation of image containing text of picture

--- n400/randomize.py
+++ n400/randomize.py
@@ -2,14 +2,19 @@ import shutil
 from glob import glob
 import os
 import random
+from PIL import Image, ImageDraw, ImageFont
 
 if __name__ == '__main__':
     src_dir = os.curdir
     dest_dir_name = 'experiment'
     filenames = []
 
+    # text image params
+    W, H = (400, 400)
+    font = ImageFont.truetype("arial.ttf", 40)
+
     # obtain images in current cirectory
-    for ext in '*.jpg', '*.png':
+    for ext in '*.jpg', '*.png', '*.gif':
         filenames.extend(glob(ext))
 
     # create new folder (check whether old one exists)
@@ -25,7 +30,7 @@ if __name__ == '__main__':
 
     # append idxs in ascending order to filenames
     new_filenames = {filename: '{idx:03}_{filename}'.format(
-        idx=idx+1, filename=filename) for idx, filename in enumerate(filenames)}
+        idx=2*idx, filename=filename) for idx, filename in enumerate(filenames)}
 
     # copy files to new folder
     for old_filename, new_filename in new_filenames.items():
@@ -37,3 +42,18 @@ if __name__ == '__main__':
         src_file = os.path.join(dest_dir, old_filename)
         dest_file = os.path.join(dest_dir, new_filename)
         os.rename(src_file, dest_file)
+
+        # create image with text
+        image = Image.new("RGBA", (W, H))
+        draw = ImageDraw.Draw(image)
+
+        text = old_filename.split('.')[0]  # remove extension
+        w, h = draw.textsize(text)
+        draw.text(((W-w)/2, (H-h)/2), text, fill="black", font=font)
+
+        prefix, sufix = new_filename.split('_')
+        prefix = int(prefix) + 1
+        new_filename = '{prefix:03}_{filename}'.format(
+            prefix=prefix, filename=sufix)
+        dest_file = os.path.join(dest_dir, new_filename)
+        image.save(dest_file, "PNG")
