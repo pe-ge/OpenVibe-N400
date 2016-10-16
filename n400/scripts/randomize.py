0d8102f0d8003c02260ff365052132d100c99233
pe-ge <gergelp@gmail.com>
2016-10-16 14:38:51 +0200

    recursively iterating over directories

--- n400/scripts/randomize.py
+++ n400/scripts/randomize.py
@@ -1,5 +1,5 @@
 import shutil
-from glob import glob
+import glob
 import os
 import random
 from PIL import Image, ImageDraw, ImageFont
@@ -9,13 +9,13 @@ if __name__ == '__main__':
     dest_dir_name = 'experiment'
     filenames = []
 
+    # iterate recursively over images inside dataset/
+    for filename in glob.iglob('dataset/**/*.*', recursive=True):
+        filenames.append(filename)
+
     # text image params
     W, H = (400, 400)
-    font = ImageFont.truetype("arial.ttf", 40)
-
-    # obtain images in current cirectory
-    for ext in '*.jpg', '*.png', '*.gif':
-        filenames.extend(glob(ext))
+    font = ImageFont.truetype("verdana.ttf", 40)
 
     # create new folder (check whether old one exists)
     count = 1
@@ -29,8 +29,12 @@ if __name__ == '__main__':
     random.shuffle(filenames)
 
     # append idxs in ascending order to filenames
-    new_filenames = {filename: '{idx:03}_{filename}'.format(
-        idx=(2*idx + 1), filename=filename) for idx, filename in enumerate(filenames)}
+    new_filenames = {}
+    for idx, filename in enumerate(filenames):
+        directory, img_name = os.path.split(filename)
+        new_img_name = '{idx:03}_{img_name}'.format(idx=(2 * idx + 1), img_name=img_name)
+        new_filename = os.path.join(dest_dir, new_img_name)
+        new_filenames[filename] = new_filename
 
     # copy files to new folder
     for old_filename, new_filename in new_filenames.items():
@@ -39,21 +43,23 @@ if __name__ == '__main__':
         shutil.copy(src_file, dest_dir)
 
         # rename
-        src_file = os.path.join(dest_dir, old_filename)
-        dest_file = os.path.join(dest_dir, new_filename)
-        os.rename(src_file, dest_file)
+        old_filename = os.path.split(old_filename)[1]
+        old_filename = os.path.join(dest_dir, old_filename)
+        os.rename(old_filename, new_filename)
 
         # create image with text
         image = Image.new("RGBA", (W, H))
         draw = ImageDraw.Draw(image)
 
-        text = old_filename.split('.')[0]  # remove extension
-        w, h = draw.textsize(text)
-        draw.text(((W-w)/2, (H-h)/2), text, fill="black", font=font)
+        text = os.path.split(old_filename)[1].split('.')[0]  # remove extension
+        w, h = font.getsize(text)
+        draw.text(((W-w)/2, (H-h)/2), text.capitalize(), fill="black", font=font)
+        draw.text((W/2, 0), text.capitalize(), fill="black", font=font)
 
-        prefix, sufix = new_filename.split('_')
-        prefix = int(prefix) + 1
+        new_filename = os.path.split(new_filename)[1].split('_')
+        # # print(os.path.split(new_filename))
+        prefix = int(new_filename[0]) + 1
         new_filename = '{prefix:03}_{filename}'.format(
-            prefix=prefix, filename=sufix)
+                prefix=prefix, filename='_'.join(new_filename[1:]))
         dest_file = os.path.join(dest_dir, new_filename)
         image.save(dest_file, "PNG")
