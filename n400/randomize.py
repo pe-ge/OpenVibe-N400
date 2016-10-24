20ebc88d0e7ccfa235a36a1a67a11043a74d8a8b
pe-ge <gergelp@gmail.com>
2016-10-24 11:25:10 +0200

    using new set for filenames of mismatched pictures

--- n400/randomize.py
+++ n400/randomize.py
@@ -4,32 +4,17 @@ import os
 import random
 from PIL import Image, ImageDraw, ImageFont
 
-N = 100  # number of pairs
-
-
-def create_random_dict(max_items):
-    def equals(rand_keys, rand_values):
-        return any([elem[0] == elem[1] for elem in zip(rand_keys, rand_values)])
-
-    rand_keys = [2*(i+1) for i in range(max_items)]
-    random.shuffle(rand_keys)
-    rand_keys = rand_keys[::2]  # we need only half
-
-    rand_values = rand_keys[:]
-    random.shuffle(rand_values)
-
-    # we dont want any key to be equal with its value
-    while(equals(rand_keys, rand_values)):
-        random.shuffle(rand_values)
-
-    return dict(zip(rand_keys, rand_values))
+N = 10  # number of pairs
+dest_dir_name = 'experiment'
+W, H = (600, 600)  # dimension of word image
+font = ImageFont.truetype("verdana.ttf", 50)
 
 
 def load_dataset():
     return [filename for filename in glob.iglob('dataset/**/*.*', recursive=True)]
 
 
-def create_experiment_directories(src_dir, dest_dir_name):
+def create_experiment_directories(src_dir):
     count = 1
     dest_dir = os.path.join(src_dir, dest_dir_name + str(count))
     while os.path.exists(dest_dir):
@@ -43,30 +28,48 @@ def create_experiment_directories(src_dir, dest_dir_name):
     return dest_dir1, dest_dir2
 
 
-def save_cross(W, H, filename, dest_dir):
+def save_text(W, H, path, text):
+    image = Image.new("RGBA", (W, H))
+    draw = ImageDraw.Draw(image)
+    w, h = font.getsize(text)
+    draw.text(((W-w)/2, (H-h)/2), text, fill="black", font=font)
+    image.save(path, "PNG")
+
+
+def save_cross(W, H, path):
     image = Image.new("RGBA", (W, H))
     draw = ImageDraw.Draw(image)
     border = 100
     draw.line([W/2, border, W/2, H - border],  fill="black", width=10)
     draw.line([border, H/2, W - border, H/2],  fill="black", width=10)
-    image.save(os.path.join(dest_dir, filename), "PNG")
+    image.save(path, "PNG")
+
+
+def prepare_text(path):
+    text = os.path.split(path)[1].split('.')[0]  # remove extension
+    return text.lower().replace('_', ' ')
 
 
 def process_dataset(dataset, dest_dir):
-    save_cross(W, H, "000_cross.png", dest_dir)
+    save_cross(W, H, os.path.join(dest_dir, "000_cross.png"))
     # append idxs in ascending order to filenames
-    new_filenames = {}
-    for idx, filename in enumerate(dataset):
-        directory, img_name = os.path.split(filename)
+    new_paths = {}
+    for idx, path in enumerate(dataset[0:2*N]):
+        directory, img_name = os.path.split(path)
         new_img_name = '{:03}_{}'.format(2 * idx + 1, img_name)
-        new_filename = os.path.join(dest_dir, new_img_name)
-        new_filenames[filename] = new_filename
+        new_path = os.path.join(dest_dir, new_img_name)
+        new_paths[path] = new_path
 
-    filenames_map = create_random_dict(len(new_filenames))
+    # idxs of picturesthat mismatch word
+    mismatched = [False] * N + [True] * N
+    random.shuffle(mismatched)
+    m_idx = 0  # iterating over mismatched list
+    d_idx = 0  # iterating over dataset[2*N:] list
+
     # copy files to new folder
-    for old_filename, new_filename in new_filenames.items():
+    for old_filename, new_filename in new_paths.items():
         # copy
-        src_file = os.path.join(src_dir, old_filename)
+        src_file = os.path.join(os.curdir, old_filename)
         shutil.copy(src_file, dest_dir)
 
         # rename
@@ -74,40 +77,29 @@ def process_dataset(dataset, dest_dir):
         old_filename = os.path.join(dest_dir, old_filename)
         os.rename(old_filename, new_filename)
 
-        # create image with text
-        image = Image.new("RGBA", (W, H))
-        draw = ImageDraw.Draw(image)
-
-        # prepare text
-        text = os.path.split(old_filename)[1].split('.')[0]  # remove extension
-        text = text.lower()
-        text = text.replace('_', ' ')
-
-        w, h = font.getsize(text)
-        draw.text(((W-w)/2, (H-h)/2), text, fill="black", font=font)
+        if mismatched[m_idx]:
+            text = prepare_text(dataset[2*N + d_idx])
+            d_idx += 1
+        else:
+            text = prepare_text(old_filename)
+        m_idx += 1
 
         new_filename = os.path.split(new_filename)[1].split('_')
         prefix = int(new_filename[0]) + 1
-        # remap prefix
-        prefix = filenames_map.get(prefix, prefix)
 
-        new_filename = '{prefix:03}_{filename}'.format(
-                prefix=prefix, filename='_'.join(new_filename[1:]))
+        new_filename = '{prefix:03}_{filename}.png'.format(
+                prefix=prefix, filename=text)
         dest_file = os.path.join(dest_dir, new_filename)
-        image.save(dest_file, "PNG")
+        save_text(W, H, dest_file, text)
 
 if __name__ == '__main__':
-    src_dir = os.curdir
-    dest_dir_name = 'experiment'
-    W, H = (600, 600)
-    font = ImageFont.truetype("verdana.ttf", 50)
 
     full_dataset = load_dataset()
     random.shuffle(full_dataset)
-    dataset1 = full_dataset[0:N]
-    dataset2 = full_dataset[N:2*N]
+    dataset1 = full_dataset[0:3*N]
+    dataset2 = full_dataset[3*N:6*N]
 
-    dest_dir1, dest_dir2 = create_experiment_directories(src_dir, dest_dir_name)
+    dest_dir1, dest_dir2 = create_experiment_directories(os.curdir)
 
     process_dataset(dataset1, dest_dir1)
     process_dataset(dataset2, dest_dir2)
